e3295a9fe3dfb9bbeb12f0d0ff095667a34001ba
Arnon Songmoolnak <songmoolnak.arnon@sut.ac.th>
2025-02-26 00:17:52 +0700

    working with async

--- Program.Classes/Program.Class.PlcConnection.cs
+++ Program.Classes/Program.Class.PlcConnection.cs
@@ -1,13 +1,13 @@
 ﻿using System.Net.Sockets;
-using System.Text;
 
 namespace PlcConnect.Program.Classes
 {
-    class PlcConnection
+    class PlcConnection : IDisposable
     {
-        private string _ipAddress;
-        private int _port;
-        private string _command;
+        private readonly string _ipAddress;
+        private readonly int _port;
+        private readonly string _command;
+        private bool _disposed;
 
         public PlcConnection(string ipAddress, int port, string command)
         {
@@ -16,19 +16,64 @@ namespace PlcConnect.Program.Classes
             _command = command;
         }
 
-        public string SendCommand()
+        private byte[] HexStringToBytes(string hex)
         {
-            using (TcpClient client = new TcpClient(this._ipAddress, this._port))
-            using (NetworkStream stream = client.GetStream())
+            return Enumerable.Range(0, hex.Length / 2)
+                             .Select(i => Convert.ToByte(hex.Substring(i * 2, 2), 16))
+                             .ToArray();
+        }
+
+        public async Task<string> SendCommandAsync()
+        {
+            try
             {
-                var command = Encoding.ASCII.GetBytes(_command);
-                stream.Write(command, 0, command.Length);
+                using TcpClient client = new(_ipAddress, _port);
+                using NetworkStream stream = client.GetStream();
+
+                // Set timeout values for read and write
+                stream.ReadTimeout = 5000;  // 5 seconds timeout for reading
+                stream.WriteTimeout = 5000; // 5 seconds timeout for writing
+
+                byte[] command = HexStringToBytes(_command);
+
+                // Send the command to the PLC
+                await stream.WriteAsync(command, 0, command.Length);
+
+                // Read the response
                 var response = new byte[256];
-                stream.Read(response, 0, response.Length);
-                return Encoding.ASCII.GetString(response);
+                int bytesRead = await stream.ReadAsync(response, 0, response.Length);
+
+                // Return the response as a hex string
+                return BitConverter.ToString(response, 0, bytesRead).Replace("-", "");
+            }
+            catch (Exception ex)
+            {
+                // Log exceptions (e.g., connection issues, timeouts)
+                // Assuming a logger is available
+                // Logger.LogError(ex, "Error sending command to PLC");
+                return $"Error: {ex.Message}";
             }
         }
 
-    }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // Dispose managed resources here
+                }
 
+                // Dispose unmanaged resources here
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,46 +1,45 @@
 ﻿using System.CommandLine;
-
 using PlcConnect.Program.Classes;
 
-class Program
+if (args is null)
 {
-    static async Task<int> Main(string[] args)
-    {
-        var rootCommand = new RootCommand("A simple PLC communication.");
-
-        var ipAddressOption = new Option<string>(
-            "--address",
-            "IP address of PLC"
-        );
-
-        var portOption = new Option<int>(
-            "--port",
-            "Port of PLC"
-        );
-
-        var commandOption = new Option<string>(
-            "--command",
-            "Command for sending to PLC"
-        );
-
-        rootCommand.AddOption(ipAddressOption);
-        rootCommand.AddOption(portOption);
-        rootCommand.AddOption(commandOption);
-
-        rootCommand.SetHandler(static (address, port, command) =>
-        {
-            if (address != null && port != 0 && command != null)
-            {
-                var plcConnection = new PlcConnection(address, port, command);
-                Console.WriteLine(plcConnection.SendCommand());
-            }
-            else
-            {
-                throw new Exception(message: "Missing <address> or <port> or <comand>");
-            }
-
-        }, ipAddressOption, portOption, commandOption);
-
-        return await rootCommand.InvokeAsync(args);
-    }
+    throw new ArgumentNullException(nameof(args));
 }
+
+var rootCommand = new RootCommand("A simple PLC communication.");
+
+var ipAddressOption = new Option<string>(
+    "--address",
+    "IP address of PLC"
+);
+
+var portOption = new Option<int>(
+    "--port",
+    "Port of PLC"
+);
+
+var commandOption = new Option<string>(
+    "--command",
+    "Command for sending to PLC"
+);
+
+rootCommand.AddOption(ipAddressOption);
+rootCommand.AddOption(portOption);
+rootCommand.AddOption(commandOption);
+
+rootCommand.SetHandler(async (address, port, command) =>
+{
+    if (address != null && port != 0 && command != null)
+    {
+        var plcConnection = new PlcConnection(address, port, command);
+        Console.WriteLine(await plcConnection.SendCommandAsync());
+    }
+    else
+    {
+        Console.WriteLine($"Address: {address}, Port: {port}, Command: {command}");
+        throw new Exception(message: "Missing <address> or <port> or <command>");
+    }
+
+}, ipAddressOption, portOption, commandOption);
+
+return await rootCommand.InvokeAsync(args);
