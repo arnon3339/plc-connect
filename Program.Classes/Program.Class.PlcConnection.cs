a07ec2216283696df6b3a7179e104e596ca387c8
Arnon Songmoolnak <songmoolnak.arnon@sut.ac.th>
2025-02-25 20:49:38 +0700

    first

---
+++ Program.Classes/Program.Class.PlcConnection.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace PlcConnect.Program.Classes
+{
+    class PlcConnection
+    {
+        private string _ipAddress;
+        private int _port;
+        private string _command;
+
+        public PlcConnection(string ipAddress, int port, string command)
+        {
+            _ipAddress = ipAddress;
+            _port = port;
+            _command = command;
+        }
+
+        public string SendCommand()
+        {
+            using (TcpClient client = new TcpClient(this._ipAddress, this._port))
+            using (NetworkStream stream = client.GetStream())
+            {
+                var command = Encoding.ASCII.GetBytes(_command);
+                stream.Write(command, 0, command.Length);
+                var response = new byte[256];
+                stream.Read(response, 0, response.Length);
+                return Encoding.ASCII.GetString(response);
+            }
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.CommandLine;
+
+using PlcConnect.Program.Classes;
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var rootCommand = new RootCommand("A simple PLC communication.");
+
+        var ipAddressOption = new Option<string>(
+            "--address",
+            "IP address of PLC"
+        );
+
+        var portOption = new Option<int>(
+            "--port",
+            "Port of PLC"
+        );
+
+        var commandOption = new Option<string>(
+            "--command",
+            "Command for sending to PLC"
+        );
+
+        rootCommand.AddOption(ipAddressOption);
+        rootCommand.AddOption(portOption);
+        rootCommand.AddOption(commandOption);
+
+        rootCommand.SetHandler(static (address, port, command) =>
+        {
+            if (address != null && port != 0 && command != null)
+            {
+                var plcConnection = new PlcConnection(address, port, command);
+                Console.WriteLine(plcConnection.SendCommand());
+            }
+            else
+            {
+                throw new Exception(message: "Missing <address> or <port> or <comand>");
+            }
+
+        }, ipAddressOption, portOption, commandOption);
+
+        return await rootCommand.InvokeAsync(args);
+    }
+}
